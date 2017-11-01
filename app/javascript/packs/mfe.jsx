62178abaa6def2f67df3be975115820f15c26659
Pierre-Yves Le Guennec <pyves.lg@gmail.com>
2017-11-01 08:47:45 +0100

    foreman geminstall

--- app/javascript/packs/mfe.jsx
+++ app/javascript/packs/mfe.jsx
@@ -9,75 +9,59 @@ class Mfe extends React.Component {
 
 
   render() {
+    const mfeDivStyle = {
+      backgroundColor: this.props.mfe.color
+    }
 
     return (
 
-      <div style={{backgroundColor: this.props.mfe.question1}}>Hello</div>
-      // <div style="position: relative">
-      //   <div class="row">
-      //     <div class="col-xs-12 col-sm-8 col-sm-offset-2">
-      //       <div style="background: linear-gradient(-225deg, rgba(0,150,179,0.6) 0%, rgba(80,207,140,0.6) 80%); height: 5vh;"></div>
-      //     </div>
-      //   </div>
+      <div className="container">
+        <div id="mfe-container" style={mfeDivStyle}>
+          <div className="mfe-upper-bar">
+            <div className="row">
+              <div className="col-xs-12">
+                <div className="mfe-upper-bar-content"></div>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-xs-12">
+              <div className="mfe-example-1 text-center">
+                <div id="mfe-logo" className="row">
+                  <div className="col-xs-12">
+                    {/* image */}
+                  </div>
+                </div>
+                <div id="mfe-intro" className="row">
+                  <div className="col-xs-12 col-sm-10 col-sm-offset-1">
+                    <p>
+                      <b>Bonjour !</b><br/><br/>
+                      From Monday to Friday, Novexpat helps you discover, learn or fine-tune a tricky grammatical rule, some handy vocabulary and common French idioms that will make you feel like a Parisian...<br/><br/>
+                      <em>en 45 secondes chrono</em> !<br/><br/>
+                      <b>Prêt ?</b><br/><br/>
+                    </p>
+                  </div>
+                </div>
+              {/* TODAY LEARNING */}
+                <div id="mfe-learn">
+                  <div className="row">
+                    <div className="col-xs-12">
+                      <h4>#{`AUJOURD'HUI VOUS APPRENDREZ À`}</h4>
+                    </div>
+                  </div>
 
-      //   <div class="row">
-      //     <div class="col-xs-12 col-sm-8 col-sm-offset-2">
-      //       <div class="mfe-example-1 text-center" style={{backgroundColor: this.props.mfe.color}}>
-      //         <div class="row">
-      //           <div class="col-xs-12 col-sm-10 col-sm-offset-1">
-      //             // image
-      //           </div>
-      //         </div>
+                  <div className="row">
+                    <div className="col-xs-12">
+                      <h3><span className="mfe-data-under-highlights">{this.props.mfe.learn}</span></h3>
+                    </div>
+                  </div>
+                </div>
 
-      //         <div class="row">
-      //           <div class="col-xs-12 col-sm-10 col-sm-offset-1">
-      //             <p>
-      //               <b>Bonjour !</b>
-      //               From Monday to Friday, Novexpat helps you discover, learn or fine-tune a tricky grammatical rule, some handy vocabulary and common French idioms that will make you feel like a Parisian...<br/><br/>
-      //               <em>en 45 secondes chrono</em> !<br/><br/>
-      //               <b>Prêt ?</b><br/><br/>
-      //             </p>
-      //           </div>
-      //         </div>
-
-      //         <div id="mfe-learn">
-      //           <div class="row">
-      //             <div class="col-xs-12 col-sm-10 col-sm-offset-1">
-      //               <h4>AUJOURDHUI VOUS APPRENDREZ À</h4>
-      //             </div>
-      //           </div>
-
-      //           <div class="row">
-      //             <div class="col-xs-12 col-sm-10 col-sm-offset-1">
-      //               <h3><span style="border-bottom: 10px solid #FCFFFA;" dangerouslySetInnerHTML={{__html: this.props.mfe.learn}}></span></h3>
-      //             </div>
-      //           </div>
-      //         </div>
-
-      //         <div id="mfe-question">
-      //           <div class="row">
-      //             <div class="col-xs-12 col-sm-10 col-sm-offset-1">
-      //               <h4>LA QUESTION DU JOUR</h4>
-      //             </div>
-      //           </div>
-      //           <div class="row">
-      //             <div class="col-xs-12 col-sm-10 col-sm-offset-1">
-      //               <p style="margin-top: 3vh;" dangerouslySetInnerHTML={{__html: this.props.mfe.question1}}><b> :</b></p>
-      //             </div>
-      //           </div>
-      //           <div class="row">
-      //             <div class="col-xs-10 col-xs-offset-2 col-md-6 col-md-offset-4">
-      //               <ul>
-      //                 <li><b>a)</b> <span dangerouslySetInnerHTML={{__html: this.props.mfe.option1}}></span></li>
-      //                 <li><b>b)</b> <span dangerouslySetInnerHTML={{__html: this.props.mfe.option2}}></span></li>
-      //               </ul>
-      //             </div>
-      //           </div>
-      //         </div>
-      //       </div>
-      //     </div>
-      //   </div>
-      // </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
@@ -86,7 +70,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const node = document.getElementById('mfe_data')
   const data = JSON.parse(node.getAttribute('data'))
   ReactDOM.render(
-    <Mfe mfe={data} style={{backgroundColor: white}}/>,
+    <Mfe mfe={data} />,
     document.getElementById('mfe_data'),
   )
 })
